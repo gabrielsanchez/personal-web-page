9d0247ab60d4194c3a066f332e46811c72b4159d
Gabriel <gabrielsz87@gmail.com>
2015-07-02 08:52:04 -0400

    added files

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#about").click(function(){      
+        $("#wrapper").html("<h1>Gabriel Sánchez</h1><p>I'm a composer and software engineer from Mexico. I have lived in Guadalajara, Monterrey, Mexico City, San Francisco Bay Area, Armonk and I currenty live in Brooklyn. I'm the creator of <a href='http://scripthica.com'>scripthica</a> and I'm currently working towards creating new learning tools for a wide variety of audiences. I also write, teach and make audiovisual art.</p><p>Besides coding and making music, I like roller coasters, yoga, travelling, movies and going to concerts. I'm always searching for new things to learn; lately I've been interested in fencing.</p>");    
+    });
+    
+    $("#contact").click(function(){      
+        $("#wrapper").html("<h1>gabrielsz87 (at) gmail (dot) com</h1>");    
+    });
+    
+});
